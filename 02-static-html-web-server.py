e95c912cb44b2633b35a455988e6d98610fd5efd
lzx-9 <30363708+lzx-9@users.noreply.github.com>
2018-03-12 23:00:05 +0800

    Update 02-static-html-web-server.py

--- 02-static-html-web-server.py
+++ 02-static-html-web-server.py
@@ -2,26 +2,32 @@ from socket import *
 import multiprocessing
 import threading
 import re
+import os
+import sys
 
 #常量明明规则：必须全部是大写的
-HTML_ROOT_DIR = './html/'
+HTML_ROOT_DIR = sys.path[0] + '\html'
 
 def handle_client(client_socket):
     """处理客户端的请求"""
+    print(HTML_ROOT_DIR + '*' *20)
     request_data = client_socket.recv(1024)
 
-    print(request_data)
-    request_lines = request_data.splitelines()
+    print("request_data: ",request_data)
+    request_lines = request_data.decode("utf-8").splitlines()
     for line in request_lines:
         print(line)
 
     #报文解析，提取变量
     request_start_line = request_lines[0]
     file_name = re.match(r"\w+ (/[^ ]* )", str(request_start_line)).group(1)
+    print('*' * 20)
+    print(file_name)
 
     try:
         #打开文件，提取内容
-        file = open(file_name, "rb")
+        print(HTML_ROOT_DIR + file_name)
+        file = open(HTML_ROOT_DIR + file_name, "r")
         file_data = file.read()
         file.close()
     except IOError:
@@ -33,8 +39,9 @@ def handle_client(client_socket):
         request_header = "Server: my server\r\n"
         request_body = file_data
     finally:
-        response = request_start_line + str(request_header) + "/r/n" + request_body
-        client_socket.send(bytes(response), "utf-8")
+        response = request_start_line + str(request_header) + "\r\n" + request_body
+        print(response)
+        client_socket.send(bytes(response, "utf-8"))
 
     client_socket.close()
 
@@ -58,3 +65,4 @@ if __name__ == '__main__':
         handle_client_progress = multiprocessing.Process(args=(client_socket,), target=handle_client)
         handle_client_progress.start()
         client_socket.close()
+
